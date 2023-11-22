f52b5192c47bb792526d6b1d86e4dbed9556134c
Veshal Raj <veshalraj1307@gmail.com>
2023-11-22 17:46:49 +0530

    netflix intro

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+// import '../src/net'
 import { BrowserRouter as Router} from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <Router>
-    <App />
-  </Router>,
+setTimeout(
+  () => {
+
+    ReactDOM.createRoot(document.getElementById('root')).render(
+      <Router>
+        <App />
+      </Router>,
+    )
+  },
+  5400
 )
